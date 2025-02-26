078b4c08854b1d031e9015bcd9ba42cc5b6bcbc3
Jack Estes <jackestes@Jacks-MacBook-Pro-6.local>
2025-02-26 08:26:51 -0700

    Initial commit for main project

---
+++ Mission08_0110/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission08_0110.Models;
+
+namespace Mission08_0110.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
---
+++ Mission08_0110/Models/Temp.cs
@@ -0,0 +1,6 @@
+namespace Mission08_0110.Models;
+
+public class Temp
+{
+    
+}
